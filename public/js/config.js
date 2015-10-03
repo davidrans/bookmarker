8a8b273bbd63d5eadf7d8fa477c44bdd542ba3fd
David Rans <dave.g.rans@gmail.com>
2015-10-03 14:53:47 -0700

    Add missing file, fix gitignore

---
+++ public/js/config.js
@@ -0,0 +1,8 @@
+requirejs.config({
+   baseUrl: 'js/lib',
+   paths: {
+      app: '../app',
+      jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min',
+      socketio: '../../socket.io/socket.io'
+   }
+});
